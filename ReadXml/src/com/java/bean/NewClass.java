489bebf2dd5002e7b565c6db217d1f89956fee5c
Spec-Ravi2014 <ravi.brahmbhatt@spec-india.com>
2014-11-27 17:57:40 +0530

    Added New File and changed Body

--- ReadXml/src/com/java/bean/NewClass.java
+++ ReadXml/src/com/java/bean/NewClass.java
@@ -5,16 +5,7 @@ public class NewClass {
 	
 	public static void main(String[] args) {
 		
-		Date date=new  Date();
-		
-		System.out.println(date.toString());
-		
-		SimpleDateFormat simpleDateFormat=new  SimpleDateFormat("yyyy-dd" +
-				"-MM");
-		
-		System.out.println(simpleDateFormat.format(date));
-		
-		System.out.println("Change in test date class");
+		System.out.println("Created New File to track changes");
 		
 	}
 }
